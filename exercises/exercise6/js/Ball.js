0a3dda873984018fb9e96708f83714a7490cc23a
Nomad29 <alexandramelancon@outlook.com>
2018-11-01 14:46:44 -0400

    EX6: Fixed on the lines 27 and 35 the conditional errors.

--- exercises/exercise6/js/Ball.js
+++ exercises/exercise6/js/Ball.js
@@ -24,14 +24,16 @@ function Ball(x,y,vx,vy,size,speed) {
 //////////////// FIXED
 Ball.prototype.update = function () {
   // Update position with velocity
-  this.x = this.vx;
+  //////////////// FIXED
+  this.x += this.vx;
   this.y += this.vy;
 
   // Constrain y position to be on screen
   this.y = constrain(this.y,0,height-this.size);
 
   // Check for touching upper or lower edge and reverse velocity if so
-  if (this.y = 0 || this.y + this.size === height) {
+  //////////////// FIXED
+  if (this.y === 0 || this.y + this.size === height) {
     this.vy = -this.vy;
   }
 }
