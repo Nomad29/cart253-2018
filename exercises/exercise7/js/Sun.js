2b9a016d94c60e26668e072810a10400362abcbf
Nomad29 <alexandramelancon@outlook.com>
2018-11-17 04:08:43 -0500

    EX7: Updated the p5.min.js library and added the base structure with an animated/interactive sun object, plus a theme soundtrack.

--- exercises/exercise7/js/Sun.js
+++ exercises/exercise7/js/Sun.js
@@ -1,11 +1,101 @@
 // Sun
 //
-// A class to define how the sun behaves. Including a gradient color changes
-// and a cosine sine noise wobble animation.
+// A class to define how the suns behave. Includes gradient and noise
+// sine cosine animation.
 
 // Sun constructor
 //
 // Sets the properties with the provided arguments
 function Sun() {
+  this.sunGraphics = createGraphics(400, 400);
+  this.sunGraphics.parent('sun');
+  // Sets the sun gradient animation
+  this.sunGradient = canvas.drawingContext;
+  // Number of vertex in the sun circle
+  this.sunVertexResolution = 260;
+  // Size of the sun
+  this.sunRadius = 800;
+  // Sun animation base time
+  this.sunTime = 0;
+  // Fastness of the sun animation
+  this.sunTimeChange = .005;
+  // Sun base noise value for cosine
+  this.sunX = 0;
+  // Sun base noise value for sine
+  this.sunY = 0;
+  // Sun noise value
+  this.nSunVal;
+  // Sun wobbly base noise intensity
+  this.nSunInt = 0;
+  // Sun wobbly base noise amplitude
+  this.nSunAmp = 0;
+  // Sun variable for containing the animated gradient
+  this.sunGradient;
+  // Sun variable for allowing the gradient to animate
+  this.sunGradientAnim = 1;
+  // Sun X gradient
+  this.gradX;
+  // Sun Y gradient
+  this.gradY;
+  // Sun gradient
+  this.gradient;
+  // Sun first gradient color
+  this.sun1 = sun1 = color(255, 239, 101);
+  // Sun second gradient color
+  this.sun2 = sun2 = color(255, 87, 27);
+}
+
+// display() Sun
+//
+// Draw the sun as a graphic element on the screen
+Sun.prototype.display = function() {
+  // A paper white background color
+  background(245);
+  // Center the sun in the canvas
+  translate(width / 2, height / 2);
+  // Sun wobbly noise number of details
+  noiseDetail(5);
+
+  // Allow user to move the radial gradient with the mouse
+  this.gradX = -mouseX / 4 - width / 2;
+  this.gradY = -mouseY / 4 - height / 2;
+  // Creates the radial gradient
+  this.gradient = this.sunGradient.createRadialGradient(0, 0, 150, this.gradX, this.gradY, 0);
+  // Allow to animated the radial gradiant with rotation
+  rotate(map(sin(this.sunGradientAnim), -1, 1, -PI / 4, PI / 4));
+  // Adds gradient color 2
+  this.gradient.addColorStop(0, this.sun2);
+  // Adds gradient color 1
+  this.gradient.addColorStop(0.8, this.sun1);
+  // Allow to unify the radial gradient with the animations
+  this.sunGradient.fillStyle = this.gradient;
+  // Creates the animation pace
+  this.sunGradientAnim += 0.025;
+
+  // Sun wobbly changed noise intensity after clear()
+  this.nSunInt = 0.1;
+  // Sun wobbly changed noise amplitude after clear()
+  this.nSunAmp = 0.1;
+
+  // Begins the sun wobbly animation
+  beginShape();
+  // Makes the sun a circle by forming with 2 TWO_PI and apply the vertex to it
+  for (a = 0; a <= TWO_PI; a += TWO_PI / this.sunVertexResolution) {
+    // Map noise value to match the amplitude
+    this.nSunVal = map(noise(cos(a) * this.nSunInt + 0.1, sin(a) * this.nSunInt + 0.1, this.sunTime), 0.0, 0.1, 0.1, 0.11);
+
+    // Calculates the noise values for the sun X to wobbles with cosine
+    this.sunX = cos(a) * this.sunRadius * this.nSunVal;
+    // Calculates the noise values for the sun Y to wobbles with sine
+    this.sunY = sin(a) * this.sunRadius * this.nSunVal;
+
+    // The vertex of the sun are calculated by the noise lines of sunX and sunY above
+    vertex(this.sunX, this.sunY);
+  }
+  // Ends the sun wobbly animation
+  endShape(CLOSE);
+
+  // Allows the sun wobbly animation to vary with time
+  this.sunTime += this.sunTimeChange;
 
 }
--- exercises/exercise7/js/script.js
+++ exercises/exercise7/js/script.js
@@ -8,12 +8,16 @@
 
 // 0: Title Screen
 // 1: Game Screen
+// 2: End Screen
 // Variable for making possible to show the title screen, then the game screen
 var gameScreen = 0;
-// Variable to contain the DIV ID in the HTML page
+
 var canvas;
-// Variable to contain the object representing the sun
-var sun;
+
+// Variable to contain the DIV IDs in the HTML page
+var titleCanvas;
+var gameCanvas;
+
 // Variable for the logo
 var logoImage;
 // Variable for the witch
@@ -28,6 +32,44 @@ var noMusicIconImage;
 var themeSound;
 // Variables for the images
 var witchImage;
+
+// Variable for the sun object
+var sun;
+// Variable for the sun object graphic element
+var sunGraphics;
+// Number of vertex in the sun circle
+var sunVertexResolution;
+// Size of the sun
+var sunRadius;
+// Sun animation base time
+var sunTime;
+// Fastness of the sun animation
+var sunTimeChange;
+// Sun base noise value for cosine
+var sunX;
+// Sun base noise value for sine
+var sunY;
+// Sun noise value
+var nSunVal;
+// Sun wobbly base noise intensity
+var nSunInt;
+// Sun wobbly base noise amplitude
+var nSunAmp;
+// Sun X gradient
+var gradX;
+// Sun Y gradient
+var gradY;
+// Sun gradient
+var gradient;
+// Sun first gradient color
+var sun1;
+// Sun second gradient color
+var sun2;
+// Sun variable for containing the animated gradient
+var sunGradient;
+// Sun variable for allowing the gradient to animate
+var sunGradientAnim;
+
 
 // preload()
 //
@@ -51,10 +93,14 @@ function preload() {
 //
 // Creates the ball and paddles
 function setup() {
-  createCanvas(windowWidth,windowHeight);
-
-  // Create a sun
-  sun = new Sun(width / 2, height / 2, 5, 5, 15, 5);
+  // Default canvas size with windows size
+  canvas = createCanvas(windowWidth, windowHeight);
+  // Default white paper background color
+  background(245);
+  // Erase the default black stroke
+  noStroke();
+  // Creates the sun graphics
+  sun = new Sun(width / 2, height / 2, mouseX, mouseY);
 }
 
 // draw()
@@ -62,29 +108,42 @@ function setup() {
 // Handles input, updates all the elements, checks for collisions
 // and displays everything.
 function draw() {
-  // Title Screen()
-  //
-  // Code for the title screen
-  if (gameScreen == 0) {
-    // A paper white background color
-    background(245);
-  }
 
-  // Game Screen()
-  //
-  // Code for the game screen to write in a startGame() function
+  themeSound.play();
+
+  switch (gameScreen) {
+    case 0:
+      // Title screen
+      titleScreen();
+      break;
+
+    case 1:
+      // Game screen
+      startGame();
+      break;
+
+    case 2:
+      // End screen
+      endGame();
+      break;
+  }
 
 }
 
-
-// mouseClicked()
+// titleScreen()
 //
-// For the game to be able to start.
-function mousePressed() {
-  // If the user is on the title screen and click, the game will start
-  if (gameScreen == 0) {
-    startGame();
-  }
+// This method sets the necessary variables to start the game
+function titleScreen() {
+  // Creates the title canvas to windows size
+  titleCanvas = createCanvas(windowWidth, windowHeight);
+  // Loads the backgrounds to the DIV ID named title-container in the HTML page
+  titleCanvas.parent('title-container');
+  // A paper white background color
+  background(245);
+  // Erase the default black stroke
+  noStroke();
+  // Display the sun object
+  sun.display();
 }
 
 
@@ -92,10 +151,29 @@ function mousePressed() {
 //
 // This method sets the necessary variables to start the game
 function startGame() {
-  gameScreen = 1;
-  var canvas = createCanvas(windowWidth,windowHeight);
+  // Creates the game canvas to windows size
+  gameCanvas = createCanvas(windowWidth, windowHeight);
   // Loads the backgrounds to the DIV ID named game-container in the HTML page
-  canvas.parent('game-container');
-  // Erase the default black stroke of the paddles and the ball
+  gameCanvas.parent('game-container');
+  // A black background color in the wait of developping the game screen in Exercice 8
+  background(0);
+  // Erase the default black stroke
   noStroke();
+}
+
+
+// mouseClicked()
+//
+// For the game to be able to start.
+function mousePressed() {
+  // If the user is on the title screen and click, the title screen will clear
+  if (gameScreen = 0) {
+    // Remove whole sketch on mouse press
+    remove();
+  }
+  // If the user is on the game screen, the startGame function will appear
+  else if (gameScreen = 1) {
+    startGame();
+  }
+
 }
